e870e3c6e9ae9c90939123d4fc286d29bc87a597
MiraeNassr <miraenassr@gmail.com>
2025-10-15 14:34:34 +0200

    Add if ,try and catch

--- vue-project/back-end/get_products.php
+++ vue-project/back-end/get_products.php
@@ -16,3 +16,51 @@ $category = isset($_GET['category']) ? $_GET['category'] : '';
 $sql = "SELECT product_id, naam, bescrijving, prijs, afbeelding_url, category FROM product";
 $params = []; //array to store parameters
 $where_clauses = [];
+
+// if a category is specified, add the filtering condition
+if(!empty($category)) {
+    $where_clauses[] = "category = ?";
+    $params[] =  $category;
+}
+
+//Append the WHERE clause to the sql query
+if(!empty($where_clauses)) {
+    $sql .= "WHERE " . implode("AND ", $where_clauses);
+}
+
+//connect to the database and execute the queryy
+try{
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if ($conn->connect_error) {
+        throw new Exception("Connection faild: " . $conn->connect_error);
+    }
+    $stmt = $conn->prepare($sql);
+    if ($stmt === false) {
+        throw new Exception("Prepare failed: " . $conn->error);
+    }
+    //parameters
+    if(!empty($params)) {
+        $types = str_repeat('s', count($params));
+        $stmt->bind_param($types, ...$params);
+    }
+  $stmt->execute();
+    $result = $stmt->get_result();
+
+    $products = [];
+    if ($result->num_rows > 0) {
+         while($row = $result->fetch_assoc()) {
+            $products[] = $row;
+        }
+    }
+// close the statment
+ $stmt->close();
+    $conn->close();
+
+    //return results as json
+    echo json_encode($products);
+}catch (Exception $e) {
+    //Error
+    http_response_code(500);
+     echo json_encode(['error' => $e->getMessage()]);
+}
