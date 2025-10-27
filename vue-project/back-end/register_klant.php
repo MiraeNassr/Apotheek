b15531d17c94c05a3ab490c12439c357f063e41c
MiraeNassr <miraenassr@gmail.com>
2025-10-27 11:36:08 +0100

    Build new file: register_klat.php

---
+++ vue-project/back-end/register_klant.php
@@ -0,0 +1,10 @@
+<?php 
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "De Pillen All-stars";
+
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *'); 
+http_response_code(400);
